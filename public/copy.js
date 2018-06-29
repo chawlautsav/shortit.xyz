e2c026b37a0bfce7543ff117db341728700e4f20
chawlautsav <chawlautsav@gmail.com>
2018-06-30 01:06:27 +0530

    extra

--- public/copy.js
+++ public/copy.js
@@ -1,6 +1,9 @@
-function copy() {
+ function copy() {
+
+
 	var copyText = document.getElementById("createdurl");
 	copyText.select();
 	document.execCommand("copy");
-  }
+	alert("Copied the text: " + copyText.value);
+}
 						// Updated 29-06-2018
